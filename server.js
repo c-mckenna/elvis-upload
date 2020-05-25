e06c989c21720880e9bc8178738a9ea723f0a2a2
Larry Hill <tomella1@gmail.com>
2020-05-25 17:24:25 +0930

    Convert to secrets instead of system variables for server.

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ const config = require("./lib/config");
 const express = require("express");
 const fileUpload = require("express-fileupload");
 
-const S3 = require("./lib/s3");
-const awsS3 = new S3(config);
-
-const Cognito = require("./lib/cognito");
-const cognito = new Cognito(config.cognito);
+const SecretsManager = require(".lib/secretsmanager");;
+let secretsmanager = new SecretsManager(config.secretsManager.region);
 
 const app = express();
-app.use(fileUpload());
 
 const yargs = require('yargs').options({
    'port': {
@@ -34,69 +30,93 @@ const yargs = require('yargs').options({
 });
 const argv = yargs.argv;
 const port = process.env.PORT || argv.port;
+const Cognito = require("./lib/cognito");
+const S3 = require("./lib/s3");
 
-// eventually this mime type configuration will need to change
-// https://github.com/visionmedia/send/commit/d2cb54658ce65948b0ed6e5fb5de69d022bef941
-const mime = express.static.mime;
-mime.define({
-   'application/json': ['czml', 'json', 'geojson', 'topojson'],
-   'model/vnd.gltf+json': ['gltf'],
-   'model/vnd.gltf.binary': ['bgltf'],
-   'text/plain': ['glsl']
-});
+let awsS3;
 
-app.use('/login', function (req, res) {   // Allows access to login page
-   res.sendFile(__dirname + '/dist/login.html');  // before access token check
-});
+let cognito;
+let secrets;
+let loginUrl;
 
-app.get('/', redirectSansToken);
-app.get('/index.html', redirectSansToken);
-
-function redirectSansToken(req, res) {
-   // console.log(req.query);
-   cognito.validate(req.query.access_token).then(response => {
-      res.cookie('placenamesUpload',
-         "username=" + response.username +
-         ';jurisdiction=' + response['cognito:groups'][0] +
-         ";expires=" + response.exp +
-         ";token=" + req.query.access_token,
-         { expires: new Date(response.exp * 1000) });
-
-      console.log(response);
-      res.sendFile(__dirname + "/dist/index.html")
-   }).catch((error) => {
-      console.log("Going to login page: ", error);
-      res.redirect(config.loginUrl);
+(async function () {
+   secrets = await secretsmanager.getSecretValue(config.secretsManager.secretName);
+   cognito = new Cognito({
+      jwksUrl: secrets[config.jwksUrl]
    });
-}
+   awsS3 = new S3(secrets[config.bucket]);
+   loginUrl = secrets[config.loginUrl];
 
-// serve static files
-app.use(express.static("dist"));
-app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-});
 
-// The app is sessionless so we validate the token on upload. If they try to spoof something along the way we should catch it here.
-app.post('/upload', function (req, res, next) {
-   let token = req.query.token;
-   cognito.validate(token).then(response => {
-      let file = req.files.file;
-      let jurisdiction = response['cognito:groups'][0];
-      let prefix = config.jurisdictionToS3[jurisdiction].path;
+   app.use(fileUpload());
 
-      awsS3.upload(prefix, file.name, file.data).then(result => {
-         console.log("" + new Date() + "; " + response.username + "; " + file.size + " bytes; " + JSON.stringify(result));
-         res.status(200).send("Message");
-      }).catch(e => {
-         res.status(500).send(e);
+   // eventually this mime type configuration will need to change
+   // https://github.com/visionmedia/send/commit/d2cb54658ce65948b0ed6e5fb5de69d022bef941
+   const mime = express.static.mime;
+   mime.define({
+      'application/json': ['czml', 'json', 'geojson', 'topojson'],
+      'model/vnd.gltf+json': ['gltf'],
+      'model/vnd.gltf.binary': ['bgltf'],
+      'text/plain': ['glsl']
+   });
+
+   app.use('/login', function (req, res) {   // Allows access to login page
+      res.sendFile(__dirname + '/dist/login.html');  // before access token check
+   });
+
+   app.get('/', redirectSansToken);
+   app.get('/index.html', redirectSansToken);
+
+   async function redirectSansToken(req, res) {
+      // console.log(req.query);
+      cognito.validate(req.query.access_token).then(response => {
+         res.cookie('placenamesUpload',
+            "username=" + response.username +
+            ';jurisdiction=' + response['cognito:groups'][0] +
+            ";expires=" + response.exp +
+            ";token=" + req.query.access_token,
+            { expires: new Date(response.exp * 1000) });
+
+         console.log(response);
+         res.sendFile(__dirname + "/dist/index.html")
+      }).catch((error) => {
+         console.log("Going to login page: ", error);
+         res.redirect(loginUrl);
       });
-   }).catch((error) => {
-      res.status(500).send(error);
-   });
-});
+   }
 
-app.listen(port, function (err) {
-   console.log("running server on port " + port);
-});
+   // serve static files
+   app.use(express.static("dist"));
+   app.use(function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+   });
+
+   // The app is sessionless so we validate the token on upload. If they try to spoof something along the way we should catch it here.
+   app.post('/upload', function (req, res, next) {
+      let token = req.query.token;
+      cognito.validate(token).then(response => {
+         let file = req.files.file;
+         let jurisdiction = response['cognito:groups'][0];
+         let prefix = config.jurisdictionToS3[jurisdiction].path;
+
+         awsS3.upload(prefix, file.name, file.data).then(result => {
+            console.log("" + new Date() + "; " + response.username + "; " + file.size + " bytes; " + JSON.stringify(result));
+            res.status(200).send("Message");
+         }).catch(e => {
+            res.status(500).send(e);
+         });
+      }).catch((error) => {
+         res.status(500).send(error);
+      });
+   });
+
+   app.listen(port, function (err) {
+      console.log("running server on port " + port);
+   });
+
+})();
+
+
+
